ee6172ffa1611e779f05efeed95b62f39d77ad03
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2022-06-02 16:55:27 -0300

    first commit

---
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-yes-no-button-group',
+  templateUrl: './yes-no-button-group.component.html',
+  styleUrls: ['./yes-no-button-group.component.scss']
+})
+export class YesNoButtonGroupComponent implements OnInit {
+  @Input() public value:string = '';
+  @Input() public label = '';
+  @Output() public valueChange = new EventEmitter<string>();
+
+  public options = YesNoButtonGroupOptions;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public activate(value: string): void{
+    this.value = value;
+    this.valueChange.emit(this.value);
+  }
+
+}
+
+enum YesNoButtonGroupOptions {
+  YES = 'yes',
+  NO = 'no'
+}
