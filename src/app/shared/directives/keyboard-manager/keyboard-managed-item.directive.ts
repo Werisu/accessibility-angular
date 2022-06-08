43f9ea143552068fff3c9f0f1b8478de6c1ae945
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2022-06-08 17:15:12 -0300

    Implementando a lógica do teclado

--- src/app/shared/directives/keyboard-manager/keyboard-managed-item.directive.ts
+++ src/app/shared/directives/keyboard-manager/keyboard-managed-item.directive.ts
@@ -1,6 +1,16 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef } from "@angular/core";
 
 @Directive({
   selector: '[appKmItem]'
 })
-export class KeyboardManagedItemDirective {}
+export class KeyboardManagedItemDirective {
+  constructor(private elementRef: ElementRef<HTMLElement>){}
+
+  public focus():void {
+    this.elementRef.nativeElement.focus();
+  }
+
+  public isFocused(): boolean{
+    return this.elementRef.nativeElement === document.activeElement;
+  }
+}
--- src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
+++ src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
@@ -12,18 +12,39 @@ export class KeyboardManagerDirective {
   public manageKeys(event: KeyboardEvent): void {
     switch (event.key) {
       case 'ArrowUp':
-        console.log(this.items);
-        console.log('up');
+        this.moveFocus(ArrowDirection.RIGHT)?.focus();
         break;
       case 'ArrowDown':
-        console.log('down');
+        this.moveFocus(ArrowDirection.LEFT)?.focus();
         break;
       case 'ArrowLeft':
-        console.log('left');
+        this.moveFocus(ArrowDirection.LEFT)?.focus();
         break;
       case 'ArrowRight':
-        console.log('right');
+        this.moveFocus(ArrowDirection.RIGHT)?.focus();
         break;
     }
   }
+
+  public moveFocus(direction: ArrowDirection): KeyboardManagedItemDirective | null {
+    const items = this.items?.toArray();
+    const curentSelectedIndex: number = items?.findIndex(item => item.isFocused()) as number;
+    if(items){
+      const targetElementFocus = items[curentSelectedIndex + direction];
+      if (targetElementFocus) {
+        return targetElementFocus;
+      }
+
+      return direction ===  ArrowDirection.LEFT
+        ? items[items?.length -1]
+        : items[0];
+    }
+
+    return null;
+  }
+}
+
+enum ArrowDirection {
+  LEFT = -1,
+  RIGHT = 1
 }
